2bf49c14013debe509abbd03de21c18962d6524c
justinjonathan15 <jtactics@yahoo.com>
2024-08-05 00:47:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import os
 from flask import Flask, request, render_template, redirect, url_for, send_file, flash
-from werkzeug.utils import secure_filename
 from dotenv import load_dotenv
+from werkzeug.utils import secure_filename
 
-# Load environment variables from the .env file
+# Load environment variables
 load_dotenv()
 
-# Retrieve environment variables
 openai_api_key = os.getenv("OPENAI_API_KEY")
 secret_key = os.getenv("SECRET_KEY")
 
@@ -25,7 +24,7 @@ app.secret_key = secret_key
 # Define the upload folder
 UPLOAD_FOLDER = 'input'
 OUTPUT_FOLDER = 'output'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'mp4', 'mov', 'avi'}
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 # Ensure the input and output directories exist
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
@@ -52,7 +51,7 @@ def upload_image():
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
-        if file and allowed_file(file.filename) and file.filename.lower().endswith(('png', 'jpg', 'jpeg')):
+        if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(filepath)
@@ -66,31 +65,6 @@ def upload_image():
                 flash('File processing failed.')
                 return redirect(request.url)
     return render_template('upload_image.html')
-
-@app.route('/upload/video', methods=['GET', 'POST'])
-def upload_video():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename) and file.filename.lower().endswith(('mp4', 'mov', 'avi')):
-            filename = secure_filename(file.filename)
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(filepath)
-            # Process the file here or call your processing script
-            os.system(f'python3 Video_AI_Keyworder.py')
-            output_file = os.path.join(app.config['OUTPUT_FOLDER'], filename)
-            # Make sure the output_file exists before sending
-            if os.path.exists(output_file):
-                return send_file(output_file, as_attachment=True)
-            else:
-                flash('File processing failed.')
-                return redirect(request.url)
-    return render_template('upload_video.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
