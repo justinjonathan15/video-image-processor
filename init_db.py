f0c96112a0d24abf9283fd749de3de425e617190
justinjonathan15 <jtactics@yahoo.com>
2024-09-02 17:45:26 -0400

    Delete init_db.py

--- init_db.py
+++
@@ -1,8 +0,0 @@
-from app import db, create_app
-
-# Create an application context
-app = create_app()
-with app.app_context():
-    db.create_all()
-    print("Database initialized successfully!")
-
